f510a6b2ad69c9af7f600acf0d6285f814602113
Tyler Zey <tyler.zey@equips.com>
2020-07-21 15:47:51 -0500

    update and remove axios

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import axios from 'axios'
-
 type FileData = {
-  fileName: string,
-  extension: string,
-  contentType: string
-}
+  fileName: string;
+  extension: string;
+  contentType: string;
+};
 
-
-const download = (blob: Blob, fileInfo: FileData): void => {
-  if (window.navigator.msSaveOrOpenBlob)
-    window.navigator.msSaveBlob(blob, `${fileInfo.fileName}${fileInfo.extension}`);
-  else {
+const download = (blob: Blob | Uint8Array, fileInfo: FileData): void => {
+  if (typeof window.navigator.msSaveOrOpenBlob === 'function') {
+    window.navigator.msSaveBlob(
+      blob,
+      `${fileInfo.fileName}${fileInfo.extension}`
+    );
+  } else {
     const a = window.document.createElement('a');
     a.href = window.URL.createObjectURL(blob);
     a.download = `${fileInfo.fileName}${fileInfo.extension}`;
@@ -18,22 +18,19 @@ const download = (blob: Blob, fileInfo: FileData): void => {
     a.click();
     window.document.body.removeChild(a);
   }
-}
-
-export const downloadAUrl = async (fileUrl: string, fileInfo: FileData) => {
-  const response = await axios({
-    url: fileUrl,
-    method: 'GET',
-    responseType: 'blob',
-  });
-
-  download(response.data, fileInfo)
-}
-
-export const downloadABlob = (fileContent: Blob, fileInfo: FileData): void => {
-  const blob = new Blob([fileContent], { type: fileInfo.contentType });
-  download(blob, fileInfo)
 };
 
+export const downloadAUrl = async (fileUrl: string, fileInfo: FileData) => {
+  const response = await fetch(fileUrl, { method: 'GET' });
+  const data = await response.blob();
 
+  download(data, fileInfo);
+};
 
+export const downloadABlob = (
+  fileContent: Blob | Uint8Array,
+  fileInfo: FileData
+): void => {
+  const blob = new Blob([fileContent], { type: fileInfo.contentType });
+  download(blob, fileInfo);
+};
